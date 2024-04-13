3ba7378ffcc6c37e6ce20b2965183d0787861dc6
NotJunnin <maxjunior29@gmail.com>
2024-04-13 16:58:30 -0300

    Continuacao

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,8 +7,8 @@ const Header = () => {
                 <h1>LOGO</h1>
             </div>
             <div className="pesquisa relative">
-                <i className='pi pi-search absolute z-index-1 right-0 top-50 -mt-2 mr-2'></i>
-                <InputText />
+                <i className='pi pi-search absolute z-index-1 right-0 top-50 -mt-2 mr-3'></i>
+                <InputText className='w-full' placeholder='Pesquisar por nome' />
             </div>
         </header>
     );
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,24 +1,109 @@
 import { Card } from "primereact/card";
 import { Button } from "primereact/button";
+import { Sidebar } from "primereact/sidebar";
+import { useEffect, useState } from "react";
+import { Dialog } from "primereact/dialog";
+import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog";
 const List = () => {
+
+    const [mostrarSidebar, setMostrarSidebar] = useState(false);
+    const [mostrarSidebarAdd, setMostrarSidebarAdd] = useState(false);
+    const [mostrarDialog, setMostrarDialog] = useState(false);
+    const [teams, setTeams] = useState([]);
+
+    function confirmacao(){
+        confirmDialog({
+            header: "Aviso",
+            message: "Deseja realmente apagar este item?",
+            acceptLabel: "Sim",
+            rejectLabel: "Não",
+            accept: () => {
+                alert("confirmou")
+            },
+            reject: () => {
+                alert("cancelou")
+            }
+        })
+    }
+
+    async function buscarTeams(){
+        const request = await fetch("http://localhost:3000/teams")
+        const response = await request.json();
+        setTeams(response);
+    }
+
+    useEffect(() => {
+        buscarTeams();
+    }, []);
+
+
     const titulo = (
-        <div className="flex justify-content-between align-items-center">
-            titulo do card
-            <i className="pi pi-eye"></i>
+        <div className="flex justify-content-between align-items-center text-lg">
+            Titulo do card
+            <i className="pi pi-eye cursor-pointer" onClick={() => setMostrarDialog(true)}></i>
         </div>
     );
     const footer = (
         <div className="flex gap-3">
-            <Button label="Adicionar" className="flex-1" />
-            <Button icon="pi pi-trash" />
+            <Button 
+            label="Adicionar" 
+            className="flex-1 px-0" 
+            onClick={() => setMostrarSidebarAdd(true)}/>
+            <Button icon="pi pi-trash" onClick={confirmacao} />
         </div>
     );
     return (
-        <>
-            <Card title={titulo} footer={footer}>
+        <section className="flex flex-wrap gap-3 px-8">
+            <h2 className="w-full flex align-items-center justify-content-between">
+                Teams
+                <Button 
+                    label="novo team"
+                    icon="pi pi-plus"
+                    onClick={() => setMostrarSidebar(true)}
+                />
+            </h2>
+            <Card style={{width: `calc(20% - 13px)`}} title={titulo} footer={footer}>
                 <h1 className="mx-auto flex flex-column text-center">0<span className="text-sm">/ 0</span></h1>
             </Card>
-        </>
+            <Card style={{width: `calc(20% - 13px)`}} title={titulo} footer={footer}>
+                <h1 className="mx-auto flex flex-column text-center">0<span className="text-sm">/ 0</span></h1>
+            </Card>
+            <Card style={{width: `calc(20% - 13px)`}} title={titulo} footer={footer}>
+                <h1 className="mx-auto flex flex-column text-center">0<span className="text-sm">/ 0</span></h1>
+            </Card>
+            <Card style={{width: `calc(20% - 13px)`}} title={titulo} footer={footer}>
+                <h1 className="mx-auto flex flex-column text-center">0<span className="text-sm">/ 0</span></h1>
+            </Card>
+            <Card style={{width: `calc(20% - 13px)`}} title={titulo} footer={footer}>
+                <h1 className="mx-auto flex flex-column text-center">0<span className="text-sm">/ 0</span></h1>
+            </Card>
+            <Card style={{width: `calc(20% - 13px)`}} title={titulo} footer={footer}>
+                <h1 className="mx-auto flex flex-column text-center">0<span className="text-sm">/ 0</span></h1>
+            </Card>
+
+            <Sidebar 
+                visible={mostrarSidebar}
+                onHide={() => setMostrarSidebar(false)}
+                position="right"
+            >
+                Teste
+            </Sidebar>
+            <Sidebar 
+                visible={mostrarSidebarAdd}
+                onHide={() => setMostrarSidebarAdd(false)}
+                position="left"
+            >
+                Adicionar Cobaia
+            </Sidebar>
+            <Dialog 
+                visible={mostrarDialog}
+                onHide={() => setMostrarDialog(false)}
+                position="top"
+            >
+                Lista de nomes do time
+            </Dialog>
+            <ConfirmDialog />
+        </section>
     );
 }
  
